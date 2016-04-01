0df8494f3d0320fdc746b0ed9cdade09dd201408
JakubL <jakub.lewalski@gmail.com>
2016-04-01 14:15:30 +0200

    Add sd files, fix spi transfer

---
+++ sd.h
@@ -0,0 +1,13 @@
+#ifndef __SD_H
+#define __SD_H
+
+#include <avr/io.h>
+
+#define CS_DDR  DDRB
+#define CS_ENABLE() (PORTB &= ~CS)
+#define CS_DISABLE() (PORTB |= CS)
+
+/* Send command to SD card via SPI interface */
+extern void SD_command(uint8_t cmd, uint32_t arg, uint8_t crc, uint8_t read);
+
+#endif
